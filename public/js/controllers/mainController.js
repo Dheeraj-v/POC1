eae07e2a9a2837191e164018e324507f60e133cd
Dheeraj-v <dheeraj2k5@gmail.com>
2017-09-08 18:20:24 +0530

    Add files via upload

---
+++ public/js/controllers/mainController.js
@@ -0,0 +1,66 @@
+app.controller('mainCtrl',function($scope,$http){
+    $scope.viewItems = 1;
+    $scope.menuItems = ['home','chicken', 'mutton', 'fish']
+    $scope.activeMenu = $scope.menuItems[0];
+    console.log($scope.menuItems)
+   $scope.setActive = function(menuItem) {
+    $scope.activeMenu = menuItem
+ }
+
+ 
+    
+  
+  //add
+ 	$scope.addToCart = function(event,ajaxCall){
+    $scope.refresh();
+    var canBeAdded=  true;
+    $scope.quantity = 1
+ 		$scope.itemName = angular.element(event.target).data('id')
+ 		$scope.itemPrice = angular.element(event.target).data('amount')
+    $scope.totalPrice = parseInt($scope.itemPrice) * parseInt($scope.quantity)
+   var cartsTofilled = {
+      itemName: $scope.itemName,
+      price:$scope.itemPrice,
+      totalPrice:$scope.totalPrice,
+      quantity:$scope.quantity
+   }
+    angular.forEach($scope.cartdetaillists, function(value, key){
+      if(value.productname == cartsTofilled.itemName){
+        canBeAdded=  false;
+      }
+    });
+    if(canBeAdded == true){
+    $http.post('/users', cartsTofilled).success(function(response) {
+      console.log(response);
+    });
+    }
+ 	}
+
+  //load
+  $scope.refresh = function(){
+    $http.get('/users').success(function(response) {
+      console.log(response)
+      $scope.cartdetaillists = response;
+    });
+  }
+
+  $scope.refresh();
+
+// delete
+  $scope.remove = function(id) {
+  $http.delete('/users/'+id).success(function(response) {
+   $scope.refresh();
+  });
+};
+
+//update
+$scope.update = function(cartdetaillist){
+   cartdetaillist.totalprice = cartdetaillist.productamount * cartdetaillist.quantity;
+    $http.put('/users/' + cartdetaillist._id, cartdetaillist).success(function(response) {
+     $scope.refresh();
+  })
+
+}
+
+});
+
---
+++ public/js/script.js
@@ -0,0 +1,40 @@
+var app = angular.module('App', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  
+  $routeProvider
+    .when('/', {
+      templateUrl : 'home.ejs',
+      controller : 'homeCtrl'
+    })
+    
+    // saved
+    .when('/chicken', {
+      templateUrl : 'chicken.ejs',
+      controller : 'chickenCtrl'
+    })
+        // saved
+    .when('/mutton', {
+      templateUrl : 'mutton.ejs',
+      controller : 'chickenCtrl'
+    })
+        // saved
+    .when('/fish', {
+      templateUrl : 'fish.ejs',
+      controller : 'chickenCtrl'
+    })
+    //home
+    .when('/home', {
+      templateUrl : 'home.ejs',
+      controller : 'homeCtrl'
+    })
+    .when('/cart', {
+      templateUrl : 'cart.ejs',
+      controller : 'homeCtrl'
+    })
+    .when('/:itemName', {
+      templateUrl : 'product.ejs',
+      controller : 'productCtrl'
+    })            
+});
+
